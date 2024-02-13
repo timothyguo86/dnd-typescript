fdbb072128c440fda28be4dfd11574c172c4dbd1
Timothy Guo <timothyguo86@gmail.com>
2024-02-12 21:16:12 -0500

    (feat) Render form elements in browser

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log('Hello World')
+class ProjectInput {
+  private templateElement: HTMLTemplateElement
+  private hostElement: HTMLDivElement
+  private element: HTMLFormElement
+
+  constructor() {
+    this.templateElement = document.querySelector<HTMLTemplateElement>('#project-input')!
+    this.hostElement = document.querySelector<HTMLDivElement>('#app')!
+
+    const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = importedNode.firstElementChild as HTMLFormElement
+
+    this.attach()
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element)
+  }
+}
+
+const projInput = new ProjectInput()
