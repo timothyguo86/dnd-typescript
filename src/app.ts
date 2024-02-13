881a556d85a52aa3d7823e62d11b29fff7044b48
Timothy Guo <timothyguo86@gmail.com>
2024-02-13 00:28:00 -0500

    [refactor] Refactor input validation

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,57 @@
 /**
+ * Interface representing the structure of validatable input.
+ */
+interface Validatable {
+  value: string | number
+  required?: boolean
+  minLength?: number
+  maxLength?: number
+  min?: number
+  max?: number
+}
+
+/**
+ * Function to validate the input based on the Validatable interface.
+ * @param validatableInput The input to validate.
+ * @returns True if the input is valid, false otherwise.
+ */
+function validateInput(validatableInput: Validatable): boolean {
+  let isValid = true
+
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0
+  }
+  if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+    isValid = isValid && validatableInput.value.length >= validatableInput.minLength
+  }
+  if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+    isValid = isValid && validatableInput.value.length <= validatableInput.maxLength
+  }
+  if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value >= validatableInput.min
+  }
+  if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value <= validatableInput.max
+  }
+
+  return isValid
+}
+
+/**
  * ProjectInput class manages the creation and behavior of a project input form.
  * It provides methods to fetch user input, validate it, and handle form submission.
  */
 class ProjectInput {
   // Reference to the template element for the project input form
   private readonly templateElement: HTMLTemplateElement
-
   // Reference to the host element where the form will be inserted
   private readonly hostElement: HTMLDivElement
-
   // Reference to the form element itself
   private readonly formElement: HTMLFormElement
-
   // Reference to the input element for the project title
   public readonly titleInputElement: HTMLInputElement
-
   // Reference to the textarea element for the project description
   public readonly descriptionElement: HTMLTextAreaElement
-
   // Reference to the input element for the number of people assigned to the project
   public readonly peopleInputElement: HTMLInputElement
 
@@ -28,20 +62,16 @@ class ProjectInput {
   constructor() {
     // Fetch the template element from the DOM
     this.templateElement = document.querySelector<HTMLTemplateElement>('#project-input')!
-
     // Fetch the host element where the form will be inserted
     this.hostElement = document.querySelector<HTMLDivElement>('#app')!
-
     // Clone the template content, assign it to the form element, and assign an ID to the form element
     const importedNode = document.importNode(this.templateElement.content, true)
     this.formElement = importedNode.firstElementChild as HTMLFormElement
     this.formElement.id = 'user-input'
-
     // Fetch references to input elements within the form
     this.titleInputElement = this.getElement<HTMLInputElement>('#title')
     this.descriptionElement = this.getElement<HTMLTextAreaElement>('#description')
     this.peopleInputElement = this.getElement<HTMLInputElement>('#people')
-
     // Configure and attach form element
     this.configureForm()
     this.attachForm()
@@ -62,12 +92,31 @@ class ProjectInput {
    */
   private getUserInput(): [string, string, number] | void {
     // Fetch values from input fields and trim whitespace
-    const enteredTitle = this.titleInputElement.value.trim()
-    const enteredDescription = this.descriptionElement.value.trim()
-    const enteredPeople = +this.peopleInputElement.value.trim()
+    const enteredTitle = this.titleInputElement.value
+    const enteredDescription = this.descriptionElement.value
+    const enteredPeople = +this.peopleInputElement.value
+
+    const titleValidateable: Validatable = {
+      value: enteredTitle,
+      required: true
+    }
+    const descriptionValidateable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5
+    }
+    const peopleValidateable: Validatable = {
+      value: enteredPeople,
+      required: true,
+      min: 1
+    }
 
     // Validate input and return an array or void
-    if (!enteredTitle || !enteredDescription || !enteredPeople) {
+    if (
+      !validateInput(titleValidateable) ||
+      !validateInput(descriptionValidateable) ||
+      !validateInput(peopleValidateable)
+    ) {
       alert('Invalid input, please try again!')
       return
     } else {
