0a9e327a725008f91341fb7d54c7a1b5f7d4dea5
Timothy Guo <timothyguo86@gmail.com>
2024-02-12 22:47:52 -0500

    (feat) Added methods to handle form submission

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,68 @@
 class ProjectInput {
-  private templateElement: HTMLTemplateElement
-  private hostElement: HTMLDivElement
-  private element: HTMLFormElement
+  private readonly templateElement: HTMLTemplateElement
+  private readonly hostElement: HTMLDivElement
+  private readonly formElement: HTMLFormElement
+  public readonly titleInputElement: HTMLInputElement
+  public readonly descriptionElement: HTMLTextAreaElement
+  public readonly peopleInputElement: HTMLInputElement
 
   constructor() {
     this.templateElement = document.querySelector<HTMLTemplateElement>('#project-input')!
     this.hostElement = document.querySelector<HTMLDivElement>('#app')!
 
     const importedNode = document.importNode(this.templateElement.content, true)
-    this.element = importedNode.firstElementChild as HTMLFormElement
+    this.formElement = importedNode.firstElementChild as HTMLFormElement
+    this.formElement.id = 'user-input'
 
-    this.attach()
+    this.titleInputElement = this.getElement<HTMLInputElement>('#title')
+    this.descriptionElement = this.getElement<HTMLTextAreaElement>('#description')
+    this.peopleInputElement = this.getElement<HTMLInputElement>('#people')
+
+    this.configureForm()
+    this.attachForm()
   }
 
-  private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin', this.element)
+  private getElement<T extends HTMLElement>(selector: string): T {
+    return this.formElement.querySelector<T>(selector)!
+  }
+
+  private getUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value.trim()
+    const enteredDescription = this.descriptionElement.value.trim()
+    const enteredPeople = +this.peopleInputElement.value.trim()
+
+    if (!enteredTitle || !enteredDescription || !enteredPeople) {
+      alert('Invalid input, please try again!')
+      return
+    } else {
+      return [enteredTitle, enteredDescription, enteredPeople]
+    }
+  }
+
+  private clearForm() {
+    this.titleInputElement.value = ''
+    this.descriptionElement.value = ''
+    this.peopleInputElement.value = ''
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault()
+    const userInput = this.getUserInput()
+
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput
+      console.log(title, description, people)
+    }
+
+    this.clearForm()
+  }
+
+  private configureForm() {
+    this.formElement.addEventListener('submit', (e: Event) => this.submitHandler(e))
+  }
+
+  private attachForm() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.formElement)
   }
 }
 
