f0285296328b16a045101bff54e982db029da588
Timothy Guo <timothyguo86@gmail.com>
2024-02-17 14:31:44 -0500

    [feat] Add ProjectItem class for rendering the project list item

--- src/app.ts
+++ src/app.ts
@@ -109,6 +109,26 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void
 }
 
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id)
+    this.project = project
+
+    this.configure()
+    this.renderContent()
+  }
+
+  configure() {}
+
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title
+    this.element.querySelector('h3')!.textContent = this.project.people.toString()
+    this.element.querySelector('p')!.textContent = this.project.description
+  }
+}
+
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   private assignedProjects: Project[]
 
@@ -144,9 +164,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     const listEl = document.getElementById(`${this.type}-project-list`)! as HTMLUListElement
     listEl.innerHTML = ''
     for (const projectItem of this.assignedProjects) {
-      const listItem = document.createElement('li')
-      listItem.textContent = projectItem.title
-      listEl.appendChild(listItem)
+      new ProjectItem(this.element.id, projectItem)
     }
   }
 }
