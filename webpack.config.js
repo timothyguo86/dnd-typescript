23b9f530294e43f45a4bcda846709493709dab06
Timothy Guo <timothyguo86@gmail.com>
2024-02-18 00:02:23 -0500

    [feat] Replace lite-server with webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: './src/app.ts',
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname)
+      }
+    ]
+  },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/'
   },
   devtool: 'inline-source-map',
   module: {
